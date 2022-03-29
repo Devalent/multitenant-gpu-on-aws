369c7c62008bac4da1c12333fd1d48459a48a58f
Artem Abashev <artem@abashev.com>
2022-03-29 14:43:25 +0400

    Add image cache.

--- application/deploy.mjs
+++ application/deploy.mjs
@@ -3,7 +3,9 @@
 const repository = 'artema';
 const image = 'multitenant-gpu';
 const tag = 'latest';
+const fullName = `${repository}/${image}:${tag}`;
 
-await $`docker build --platform linux/amd64 -t ${image} .`;
-await $`docker tag ${image} ${repository}/${image}:${tag}`;
-await $`docker push ${repository}/${image}:${tag}`;
+await $`docker pull ${fullName} || true`;
+await $`docker build --platform linux/amd64 --cache-from ${fullName} -t ${image} .`;
+await $`docker tag ${image} ${fullName}`;
+await $`docker push ${fullName}`;
