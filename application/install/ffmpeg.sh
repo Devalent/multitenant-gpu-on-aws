45c997ab71f1b7144c8dc041b147dd661e2ec6d4
Artem Abashev <artem@abashev.com>
2022-03-30 12:19:49 +0400

    Disable unused codecs.

--- application/install/ffmpeg.sh
+++ application/install/ffmpeg.sh
@@ -19,28 +19,12 @@ apt-get update -y && apt-get -y install autoconf automake build-essential libass
     && make -j$(nproc) install \
     && make -j$(nproc) distclean \
     && cd ~/ffmpeg_sources \
-    && git clone http://git.videolan.org/git/x264.git \
-    && cd x264 \
-    && PATH="/opt/ffmpeg/bin:$PATH" ./configure --prefix="/opt/ffmpeg_build" --enable-static --enable-shared \
-    && PATH="/opt/ffmpeg/bin:$PATH" make -j$(nproc) VERBOSE=1 \
-    && make -j$(nproc) install VERBOSE=1 \
-    && make -j$(nproc) distclean \
-    && cd ~/ffmpeg_sources \
     && git clone https://bitbucket.org/multicoreware/x265_git.git \
     && cd ~/ffmpeg_sources/x265_git/build/linux \
     && PATH="/opt/ffmpeg/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="/opt/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source \
     && make -j$(nproc) VERBOSE=1 \
     && make -j$(nproc) install VERBOSE=1 \
     && make -j$(nproc) clean VERBOSE=1 \
-    && cd ~/ffmpeg_sources \
-    && wget -O fdk-aac.tar.gz https://github.com/mstorsjo/fdk-aac/tarball/master \
-    && tar xzvf fdk-aac.tar.gz \
-    && cd mstorsjo-fdk-aac* \
-    && autoreconf -fiv \
-    && ./configure --prefix="/opt/ffmpeg_build" --disable-shared \
-    && make -j$(nproc) \
-    && make -j$(nproc) install \
-    && make -j$(nproc) distclean \
     && echo /opt/ffmpeg_build/lib > /etc/ld.so.conf.d/ffmpeg.conf \
     && ldconfig -vvvv \
     && cd ~/ffmpeg_sources \
@@ -65,8 +49,6 @@ apt-get update -y && apt-get -y install autoconf automake build-essential libass
         --enable-opencl \
         --enable-gpl \
         --cpu=native \
-        --enable-libfdk-aac \
-        --enable-libx264 \
         --enable-openssl \
         --enable-librtmp \
         --extra-libs="-lpthread -lm -lz" \
